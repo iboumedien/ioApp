5d261f048d406c5087de195cf72e209c7bef0b28
boumedien <iboumedien@gmail.com>
2015-07-17 14:19:53 +0200

    ioApp based on SocketIO Chat Sample

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 'use strict';
 
 // Initialisation node modules
-var express = require('express');
-var path = require('path');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-//Initialise the app
-var app = new express();
-console.log("I'm ioApp");
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(3000);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
